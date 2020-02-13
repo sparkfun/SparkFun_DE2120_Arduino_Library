92aad9ab04069ab9e0e6db3322b476a759f5f678
Nick Poole <Nick.Poole@SparkFun.com>
2020-02-13 02:24:10 -0700

    More work on library

---
+++ src/SparkFun_DE2120_Arduino_Library.cpp
@@ -0,0 +1,402 @@
+/*
+  This is a library written for the DE2120 2D Barcode Scanner Engine
+
+  SparkFun sells these at its website: www.sparkfun.com
+  
+  Do you like this library? Help support SparkFun. Buy a board!
+  https://www.sparkfun.com/products/TBD
+
+  Written by Nick Poole @ SparkFun Electronics, January 9th, 2020
+  
+  The DE2120 is a camera-based barcode scanner
+  https://github.com/sparkfun/SparkFun_DE2120_Arduino_Library
+  Development environment specifics:
+  
+  Arduino IDE 1.8.7
+  
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+  GNU General Public License for more details.
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "SparkFun_DE2120_Arduino_Library.h"
+#include "Arduino.h"
+#include "SoftwareSerial.h"
+
+SoftwareSerial serial(2,3); //(RX,TX) 
+
+//Constructor
+DE2120::DE2120()
+{
+
+}
+
+//Initializes the device with basic settings
+//Returns false if device is not detected
+boolean DE2120::begin(uint8_t baud)
+{
+  serial.begin(baud);
+	if (isConnected() == false){
+        return (false); //No device detected
+    }else{
+        return (true); //We're all setup!
+    }
+}
+
+//
+boolean DE2120::isConnected()
+{
+  if(getVersion() != 0x15) return(true); //
+  return (false);
+}
+
+//
+uint8_t DE2120::getVersion()
+{
+  sendCommand(COMMAND_GET_VERSION);
+  return(getResponse());
+}
+
+// Revert module to all factory default settings
+void DE2120::factoryDefault()
+{
+  sendCommand(COMMAND_SET_DEFAULTS);
+}
+
+// 
+void DE2120::sendCommand(char* cmd, char* arg)
+{
+  char start[] = "^_^";
+  char end[] = ".";
+  strcpy(_commandString, "");
+  strcat(_commandString, start);
+  strcat(_commandString, cmd);
+  strcat(_commandString, arg);  
+  strcat(_commandString, end);
+  serial.print(_commandString);
+}
+
+// 
+bool DE2120::getResponse()
+{  
+  if(serial.available){
+  }else{
+    return false;
+  }
+}
+
+// Change the serial baud rate for the barcode module
+void DE2120::changeBaudRate(uint16_t baud)
+{
+  uint8_t arg = 10;
+  switch(baud){
+    case 1200:
+      arg = 2;
+      break;
+    case 2400:
+      arg = 3;
+      break;
+    case 4800:
+      arg = 4;
+      break;
+    case 9600:
+      arg = 5:
+      break;
+    case 19200:
+      arg = 6;
+      break;
+    case 38400:
+      arg = 7;
+      break;
+    case 57600:
+      arg = 8;
+      break;
+    case 115200:
+      arg = 9;
+      break;
+  }
+  // only change baud rate if a valid value is passed
+  if(arg<10){
+    sendCommand(PROPERTY_BAUD_RATE, arg);
+  }
+}
+
+// Change the beep frequency between low, med, and high
+void DE2120::changeBuzzerTone(uint8_t tone)
+{
+  // only change if a valid value is passed
+  if(tone > 0 && tone < 4){
+    sendCommand(PROPERTY_BUZZER_FREQ, '0'+tone); // conv single-digit int to char by adding to '0'
+  }
+}
+
+// Enable and Disable Beep sound on successful read
+void DE2120::enableDecodeBeep()
+{
+  sendCommand(PROPERTY_DECODE_BEEP, '1');
+}
+void DE2120::disableDecodeBeep()
+{
+  sendCommand(PROPERTY_DECODE_BEEP, '0');
+}
+
+// Enable and Disable Beed sound on startup
+void DE2120::enableBootBeep()
+{
+  sendCommand(PROPERTY_BOOT_BEEP, '1');
+}
+void DE2120::disableBootBeep()
+{
+  sendCommand(PROPERTY_BOOT_BEEP, '0');
+}
+
+// Control the white illumination LED
+void DE2120::lightOn()
+{
+  sendCommand(PROPERTY_FLASH_LIGHT, '1');
+}
+void DE2120::lightOff()
+{
+  sendCommand(PROPERTY_FLASH_LIGHT, '0');
+}
+
+// Control the red scan line
+void DE2120::reticleOn()
+{
+  sendCommand(PROPERTY_AIM_LIGHT, '1');
+}
+void DE2120::reticleOff()
+{
+  sendCommand(PROPERTY_AIM_LIGHT, '0');
+}
+
+// Change the percentage of the frame to scan for barcodes
+void DE2120::changeReadingArea(uint8_t percent)
+{
+  uint8_t arg = 5;
+  switch(baud){
+    case 100:
+      arg = 0;
+      break;
+    case 80:
+      arg = 1;
+      break;
+    case 60:
+      arg = 2;
+      break;
+    case 40:
+      arg = 3:
+      break;
+    case 20:
+      arg = 4;
+      break;
+  }
+  // only change if a valid value is passed
+  if(arg<5){
+    sendCommand(PROPERTY_READING_AREA, arg);
+  }
+}
+
+// Enable and Disable Mirror Image reading
+void DE2120::enableImageFlipping()
+{
+  sendCommand(PROPERTY_MIRROR_FLIP, '1');
+}
+void DE2120::disableImageFlipping()
+{
+  sendCommand(PROPERTY_MIRROR_FLIP, '0');
+}
+
+// Enable USB Communication and set the mode
+// THIS WILL MAKE THE MODULE STOP RESPONDING ON TTL
+void DE2120::USBMode(char* mode)
+{
+  // reject invalid mode strings
+  if(mode == 'KBD' || mode == 'HID' || mode == 'VIC'){
+    sendCommand(PROPERTY_COMM_MODE, mode);
+  }
+}
+
+// Enable and disable continuous read mode 
+// if enabling, set the repeat interval for same-code reads
+void DE2120::enableContinuousRead(uint8_t repeatInterval)
+{
+  sendCommand(PROPERTY_READING_MODE, 'CNT');
+  // reject invalid parameters
+  if(repeatInterval < 4){
+    sendCommand(PROPERTY_CONTINUOUS_MODE_INTERVAL, '0'+repeatInterval); // conv single-digit int to char by adding to '0'
+  }
+}
+void DE2120::disableContinuousRead()
+{
+  sendCommand(PROPERTY_READING_MODE, 'MAN');
+}
+
+// Enable and disable motion sensitive read mode 
+// if enabling, set the sensitivity level
+void DE2120::enableMotionSense(uint8_t sensitivity)
+{
+  sendCommand(PROPERTY_READING_MODE, 'MDH');
+   // reject invalid sensitivity values
+  if(sensitivity == 15 || sensitivity == 20 || sensitivity == 30 || sensitivity == 50 || sensitivity == 100){
+    sendCommand(PROPERTY_COMM_MODE, '0'+sensitivity); // conv single-digit int to char by adding to '0'
+  } 
+}
+void DE2120::disableMotionSense()
+{
+  sendCommand(PROPERTY_READING_MODE, 'MAN');
+}
+
+// Enable or Disable decoding of all 1D symbologies
+void DE2120::enableAll1D()
+{
+  sendCommand(PROPERTY_ENABLE_ALL_1D);
+}
+void DE2120::disableAll1D()
+{
+  sendCommand(PROPERTY_DISABLE_ALL_1D);
+}
+
+// Enable or Disable decoding of all 2D symbologies
+void DE2120::enableAll2D()
+{
+  sendCommand(PROPERTY_ENABLE_ALL_2D);
+}
+void DE2120::disableAll2D()
+{
+  sendCommand(PROPERTY_ENABLE_ALL_2D);
+}
+
+// Start or stop reading when in Trigger Mode (DEFAULT)
+// Module will automatically stop reading after a few seconds
+void DE2120::startRead()
+{
+  sendCommand(COMMAND_START_SCAN);
+}
+void DE2120::stopRead()
+{
+  sendCommand(COMMAND_STOP_SCAN);
+}
+
+// Enable or Disable particular barcode symbologies
+void DE2120::enableSymbology(char* symbology)
+{
+  switch(symbology){
+    case 'UPCA':
+      sendCommand(PROPERTY_ENABLE_UPCA, '1');
+      break;
+    case 'UPCE':
+      sendCommand(PROPERTY_ENABLE_UPCE, '1');
+      break;
+    case 'EAN8':
+      sendCommand(PROPERTY_ENABLE_EAN8, '1');
+      break;
+    case 'EAN13':
+      sendCommand(PROPERTY_ENABLE_EAN13, '1');
+      break;
+    case 'CODE128':
+      sendCommand(PROPERTY_ENABLE_CODE128, '1');
+      break;
+    case 'GS1128':
+      sendCommand(PROPERTY_ENABLE_GS1128, '1');
+      break;
+    case 'CODE39':
+      sendCommand(PROPERTY_ENABLE_CODE39, '1');
+      break;
+    case 'CODE93':
+      sendCommand(PROPERTY_ENABLE_CODE93, '1');
+      break;
+    case 'CODE11':
+      sendCommand(PROPERTY_ENABLE_CODE11, '1');
+      break;
+    case 'INT2OF5':
+      sendCommand(PROPERTY_ENABLE_INT2OF5, '1');
+      break;
+    case 'MATRIX2OF5':
+      sendCommand(PROPERTY_ENABLE_MATRIX2OF5, '1');
+      break;
+    case 'GS1DATABAR':
+      sendCommand(PROPERTY_ENABLE_GS1DATABAR, '1');
+      break;
+    case 'CHINAPOST':
+      sendCommand(PROPERTY_ENABLE_CHINAPOST, '1');
+      break;
+    case 'QRCODE':
+      sendCommand(PROPERTY_ENABLE_QRCODE, '1');
+      break;
+    case 'DATAMATRIX':
+      sendCommand(PROPERTY_ENABLE_DATAMATRIX, '1');
+      break;
+    case 'PDF417':
+      sendCommand(PROPERTY_ENABLE_PDF417, '1');
+      break;
+    case 'MICROPDF417':
+      sendCommand(PROPERTY_ENABLE_MICROPDF417, '1');
+      break;
+    case 'AZTEC':
+      sendCommand(PROPERTY_ENABLE_AZTEC, '1');
+      break;
+  }
+}
+void DE2120::disableSymbology(char* symbology)
+{
+  switch(symbology){
+    case 'UPCA':
+      sendCommand(PROPERTY_ENABLE_UPCA, '0');
+      break;
+    case 'UPCE':
+      sendCommand(PROPERTY_ENABLE_UPCE, '0');
+      break;
+    case 'EAN8':
+      sendCommand(PROPERTY_ENABLE_EAN8, '0');
+      break;
+    case 'EAN13':
+      sendCommand(PROPERTY_ENABLE_EAN13, '0');
+      break;
+    case 'CODE128':
+      sendCommand(PROPERTY_ENABLE_CODE128, '0');
+      break;
+    case 'GS1128':
+      sendCommand(PROPERTY_ENABLE_GS1128, '0');
+      break;
+    case 'CODE39':
+      sendCommand(PROPERTY_ENABLE_CODE39, '0');
+      break;
+    case 'CODE93':
+      sendCommand(PROPERTY_ENABLE_CODE93, '0');
+      break;
+    case 'CODE11':
+      sendCommand(PROPERTY_ENABLE_CODE11, '0');
+      break;
+    case 'INT2OF5':
+      sendCommand(PROPERTY_ENABLE_INT2OF5, '0');
+      break;
+    case 'MATRIX2OF5':
+      sendCommand(PROPERTY_ENABLE_MATRIX2OF5, '0');
+      break;
+    case 'GS1DATABAR':
+      sendCommand(PROPERTY_ENABLE_GS1DATABAR, '0');
+      break;
+    case 'CHINAPOST':
+      sendCommand(PROPERTY_ENABLE_CHINAPOST, '0');
+      break;
+    case 'QRCODE':
+      sendCommand(PROPERTY_ENABLE_QRCODE, '0');
+      break;
+    case 'DATAMATRIX':
+      sendCommand(PROPERTY_ENABLE_DATAMATRIX, '0');
+      break;
+    case 'PDF417':
+      sendCommand(PROPERTY_ENABLE_PDF417, '0');
+      break;
+    case 'MICROPDF417':
+      sendCommand(PROPERTY_ENABLE_MICROPDF417, '0');
+      break;
+    case 'AZTEC':
+      sendCommand(PROPERTY_ENABLE_AZTEC, '0');
+      break;
+  }  
+}
---
+++ src/SparkFun_DE2120_Arduino_Library.h
@@ -0,0 +1,202 @@
+/*
+  This is a library written for the DE2120 2D Barcode Scanner Engine
+
+  SparkFun sells these at its website: www.sparkfun.com
+  
+  Do you like this library? Help support SparkFun. Buy a board!
+  https://www.sparkfun.com/products/TBD
+
+  Written by Nick Poole @ SparkFun Electronics, January 9th, 2020
+  
+  The DE2120 is a camera-based barcode scanner
+  https://github.com/sparkfun/SparkFun_DE2120_Arduino_Library
+  Development environment specifics:
+  
+  Arduino IDE 1.8.7
+  
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+  GNU General Public License for more details.
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+
+#ifndef _SPARKFUN_DE2120_ARDUINO_LIBRARY_H
+#define _SPARKFUN_DE2120_ARDUINO_LIBRARY_H
+#include "Arduino.h"
+
+//These are the commands we can send (Prepend "^_^" and append ".")
+#define COMMAND_START_SCAN "SCAN"
+#define COMMAND_STOP_SCAN "SLEEP"
+#define COMMAND_SET_DEFAULTS "DEFALT"
+#define COMMAND_GET_VERSION "DSPYFW"
+
+#define PROPERTY_BUZZER_FREQ "BEPPWM"
+//BEPPWM0 - Active Drive
+//BEPPWM1 - Passive Low Freq
+//BEPPWM2 - Passive Med Freq (default)
+//BEPPWM3 - Passive Hi Freq 
+
+#define PROPERTY_DECODE_BEEP "BEPSUC"
+//BEPSUC1 - ON (default)
+//BEPSUC0 - OFF
+
+#define PROPERTY_BOOT_BEEP "BEPPWR"
+//BEPPWR1 - ON (default) 
+//BEPPWR0 - OFF
+
+#define PROPERTY_FLASH_LIGHT "LAMENA"
+//LAMENA1 - ON (default)
+//LAMENA0 - OFF
+
+#define PROPERTY_AIM_LIGHT "AIMENA"
+//AIMENA1 - ON (default)
+//AIMENA0 - OFF
+
+#define PROPERTY_READING_AREA "IMGREG"
+//IMGREG0 - Full Width (default)
+//IMGREG1 - Center 80%
+//IMGREG2 - Center 60%
+//IMGREG3 - Center 40%
+//IMGREG4 - Center 20%
+
+#define PROPERTY_MIRROR_FLIP "MIRLRE"
+//MIRLRE1 - ON
+//MIRLRE0 - OFF (default)
+
+#define PROPERTY_USB_DATA_FORMAT "UTFEAN"
+//UTFEAN0 - GBK (default)
+//UTFEAN1 - UTF-8
+
+#define PROPERTY_SERIAL_DATA_FORMAT "232UTF"
+//232UTF0 - GBK (default)
+//232UTF1 - UTF-8
+//232UTF2 - Unicode BIG
+//232UTF3 - Unicode little
+
+#define PROPERTY_INVOICE_MODE "SPCINV"
+//SPCINV1 - ON
+//SPCINV0 - OFF (default)
+
+#define PROPERTY_VIRTUAL_KEYBOARD "KBDVIR"
+//KBDVIR1 - ON (default)
+//KBDVIR0 - OFF
+
+#define PROPERTY_COMM_MODE "POR"
+//PORKBD - USB-KBW Mode
+//PORHID - USB-HID Mode
+//PORVIC - USB-COM Mode
+//POR232 - TTL/RS232
+
+#define PROPERTY_BAUD_RATE "232BAD"
+//232BAD2 - 1200 bps
+//232BAD3 - 2400 bps
+//232BAD4 - 4800 bps
+//232BAD5 - 9600 bps
+//232BAD6 - 19200 bps
+//232BAD7 - 38400 bps
+//232BAD8 - 57600 bps
+//232BAD9 - 115200 bps (default)
+
+#define PROPERTY_READING_MODE "SCM"
+//SCMMAN - Manual (default)
+//SCMCNT - Continuous
+//SCMMDH - Motion Mode
+
+#define PROPERTY_CONTINUOUS_MODE_INTERVAL "CNTALW"
+//CNTALW0 - Output Once
+//CNTALW1 - Output Continuous No Interval
+//CNTALW2 - Output Continuous 0.5s Interval
+//CNTALW3 - Output Continuous 1s Interval
+
+#define PROPERTY_MOTION_SENSITIVITY "MDTTHR"
+//MDTTHR15 - Extremely High Sensitivity
+//MDTTHR20 - High Sensitivity (default)
+//MDTTHR30 - Highish Sensitivity
+//MDTTHR50 - Mid Sensitivity
+//MDTTHR100 - Low Sensitivity
+
+#define PROPERTY_TRANSFER_CODE_ID "CIDENA"
+//CIDENA1 - Transfer Code ID
+//CIDENA0 - Do Not Transfer Code ID (default)
+
+#define PROPERTY_KBD_CASE_CONVERSION "KBDCNV"
+//KBDCNV0 - No conversion (default)
+//KBDCNV1 - ALL CAPS 
+//KBDCNV2 - all lowercase 
+//KBDCNV3 - case-to-case
+
+// Barcode Style Enable/Disable
+#define PROPERTY_ENABLE_ALL_1D "ODCENA"
+#define PROPERTY_DISABLE_ALL_1D "ODCDIS"
+#define PROPERTY_ENABLE_ALL_2D "AQRENA"
+#define PROPERTY_DISABLE_ALL_2D "AQRDIS"
+
+// Barcode Symbologies Enable/Disable
+// 0 - disable
+// 1 - enable
+#define PROPERTY_ENABLE_UPCA "UPAENA"
+#define PROPERTY_ENABLE_UPCE "UPEENA"
+#define PROPERTY_ENABLE_EAN8 "EA8ENA"
+#define PROPERTY_ENABLE_EAN13 "E13ENA"
+#define PROPERTY_ENABLE_CODE128 "128ENA"
+#define PROPERTY_ENABLE_GS1128 "GS1ENA"
+#define PROPERTY_ENABLE_CODE39 "C39ENA"
+#define PROPERTY_ENABLE_CODE93 "C93ENA"
+#define PROPERTY_ENABLE_CODE11 "C11ENA"
+#define PROPERTY_ENABLE_INT2OF5 "I25ENA"
+#define PROPERTY_ENABLE_MATRIX2OF5 "M25ENA"
+#define PROPERTY_ENABLE_GS1DATABAR "GS1DBE"
+#define PROPERTY_ENABLE_CHINAPOST "D25ENA"
+#define PROPERTY_ENABLE_QRCODE "QRCENA"
+#define PROPERTY_ENABLE_DATAMATRIX "DMCENA"
+#define PROPERTY_ENABLE_PDF417 "PDFENA"
+#define PROPERTY_ENABLE_MICROPDF417 "MCFENA"
+#define PROPERTY_ENABLE_AZTEC "AZTENA"
+
+class DE2120 {
+  public:
+    DE2120();
+
+    boolean begin(uint8_t baud = 115200);
+    boolean isConnected(); //Returns true if device's ID is what it should be
+	  uint8_t getVersion(); //Queries device for its Version #
+    void factoryDefault(); 
+    void sendCommand(char* cmd, char* arg = "");
+    void changeBaudRate(uint16_t baud);
+    void changeBuzzerTone(uint8_t tone);
+    void enableDecodeBeep();
+    void disableDecodeBeep();
+    void enableBootBeep();
+    void disableBootBeep();
+    void lightOn();
+    void lightOff();
+    void reticleOn();
+    void reticleOff();
+    void changeReadingArea(uint8_t percent);
+    void enableImageFlipping();
+    void disableImageFlipping();
+    void USBMode(char* mode);
+    void enableContinuousRead(uint8_t repeatInterval);
+    void disableContinuousRead();
+    void enableMotionSense(uint8_t sensitivity);
+    void disableMotionSense();
+    void enableAll1D();
+    void disableAll1D();
+    void enableAll2D();
+    void disableAll2D();
+    void startRead();
+    void stopRead();
+    void enableSymbology(char* symbology);
+    void disableSymbology(char* symbology);
+
+  private:
+ 
+    char _commandString[10] = {0};
+    uint16_t _baudRate = 115200;
+
+};
+
+#endif
