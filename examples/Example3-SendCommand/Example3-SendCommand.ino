c94da60a78b60e82092b6acf9854a507f262f419
pmakin-png <54557724+pmakin-png@users.noreply.github.com>
2021-04-23 11:21:18 -0600

    Update pin connections
    TX/RX pin connections are backwards in original code

--- examples/Example3-SendCommand/Example3-SendCommand.ino
+++ examples/Example3-SendCommand/Example3-SendCommand.ino
@@ -22,15 +22,15 @@
   To connect the barcode scanner to an Arduino:
 
   (Arduino pin) = (Scanner pin)
-  3 = RX pin on scanner
-  2 = TX pin on scanner
+  2 = RX pin on scanner
+  3 = TX pin on scanner
   GND = GND
   3.3V = 3.3V
 
 */
 
 #include "SoftwareSerial.h"
-SoftwareSerial softSerial(2, 3); //RX, TX: Connect Arduino pin 2 to scanner TX pin. Connect Arduino pin 3 to scanner RX pin.
+SoftwareSerial softSerial(2, 3); //RX, TX: Connect Arduino pin 3 to scanner TX pin. Connect Arduino pin 2 to scanner RX pin.
 
 #include "SparkFun_DE2120_Arduino_Library.h" //Click here to get the library: http://librarymanager/All#SparkFun_DE2120
 DE2120 scanner;
